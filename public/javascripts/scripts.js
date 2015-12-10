2be1b256022b6a0a339a6185c059f98876c55fd5
Chris Arnesen <chris.arnesen@gmail.com>
2015-12-09 21:09:43 -0600

    4->2 spaces for indentation

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,20 +1,20 @@
 var app = angular.module('myApp', ['ngRoute']);
 
-app.config(['$routeProvider', function($routeProvider){
-    $routeProvider.when('/', {
-        templateUrl: '/pages/channels.html',
-        controller: 'channelsCtrl'
-    }).when('/wishlist', {
-        templateUrl: '/pages/wishlist.html',
-        controller: 'wishlistCtrl'
-    });
+app.config(['$routeProvider', function ($routeProvider) {
+  $routeProvider.when('/', {
+    templateUrl: '/pages/channels.html',
+    controller: 'channelsCtrl'
+  }).when('/wishlist', {
+    templateUrl: '/pages/wishlist.html',
+    controller: 'wishlistCtrl'
+  });
 }]);
 
-app.controller('channelsCtrl', ['$scope', '$http', function($scope, $http){
-    $http({
-        url: '/api/channels',
-        method: 'get'
-    }).then(function(response){
-        $scope.channels = response.data
-    });
-    }]);
+app.controller('channelsCtrl', ['$scope', '$http', function ($scope, $http) {
+  $http({
+    url: '/api/channels',
+    method: 'get'
+  }).then(function (response) {
+    $scope.channels = response.data
+  });
+}]);
